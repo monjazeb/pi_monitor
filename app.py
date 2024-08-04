39da441cb48aa42e632e8dbaf07742ccfac8c667
Alireza Monjazeb <armonj@gmail.com>
2024-08-04 14:12:48 +0330

    Index page updated

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from datetime import datetime
+from flask import Flask, render_template
+from security import authenticate
 
 app = Flask(__name__)
 
+app_data = {'name':'MMM',
+            'company': 'ARPA Medical co.',
+            'version': 'v0.1 2024'}
+
 @app.route('/')
 def index():
-    return 'Hello World'
+    page={'now': datetime.now(),'title': 'Home', 'app': app_data}
+    data={'percentage': '105 %','pressure':'3.25 PSI'}
+    page['status'] = 'alarm'
+    return render_template('index.html', page=page, data=data,)
+
+@app.route('/settings')
+def settings():
+    ...
+
+
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    if authenticate.isOK():
+        app.run(debug=True, host='0.0.0.0')
 
