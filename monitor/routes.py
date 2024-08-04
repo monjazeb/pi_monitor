c5303e52dc75c55f53b31cfd952b88b4484b40f5
Alireza Monjazeb <armonj@gmail.com>
2024-08-04 16:39:40 +0330

    update project structure, home links connected

--- monitor/routes.py
+++ monitor/routes.py
@@ -1,9 +1,9 @@
 import urllib
 from datetime import datetime
-from flask import Flask, render_template
-from security import authenticate
+from flask import render_template, flash
+from monitor import app
 
-app = Flask(__name__)
+
 MagMon = "http://10.0.2.162:5000/data"
 
 app_data = {'name':'MMM',
@@ -15,7 +15,8 @@ def index():
     page={'now': datetime.now(),'title': 'Home', 'app': app_data}
     data={'percentage': '105 %','pressure':'3.25 PSI'}
     with urllib.request.urlopen(MagMon) as f:
-        data['pressure'] = str(f.read())
+        fin = f.read()
+        data['pressure']=fin
     page['wifi_status'] = 'error'
     page['alarm_status'] = 'alarm'
     return render_template('index.html', page=page, data=data,)
@@ -40,15 +41,13 @@ def settings():
     page={'now': datetime.now(),'title': 'Home', 'app': app_data}
     return render_template('settings.html', page=page)
 
+@app.route('/wifi')
+def wifi():
+    page={'now': datetime.now(),'title': 'Home', 'app': app_data}
+    return render_template('wifi.html', page=page)
+
 @app.route('/r')
 def r():
     page={'now': datetime.now(),'title': 'Home', 'app': app_data}
     return render_template('r.html', page=page)
-
-
-
-
-if __name__ == '__main__':
-    if authenticate.isOK():
-        app.run(debug=True, host='0.0.0.0')
 
---
+++ run.py
@@ -0,0 +1,6 @@
+from monitor import app
+from monitor import security
+
+if __name__ == '__main__':
+    if security.authenticate.isOK():
+        app.run(debug=True, host='0.0.0.0')
