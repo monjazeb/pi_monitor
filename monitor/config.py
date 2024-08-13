a9cc955b8ddf9500420a6f1fe98299ee3df8ca48
Alireza Monjazeb <armonj@gmail.com>
2024-08-13 13:34:03 +0330

    test commit

--- monitor/config.py
+++ monitor/config.py
@@ -2,4 +2,6 @@ MagMon = "http://10.0.2.162:5000/api"
 
 app_data = {'name':'MMM',
             'company': 'ARPA Medical co.',
-            'version': 'v0.1 2024'}
+            'version': 'v0.1 2024',
+            'refresh': '10',
+            }
